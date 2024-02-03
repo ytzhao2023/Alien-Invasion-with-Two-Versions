4135c85b86f7216d7efc2d2748b4b22960c82197
ytzhao <144186671+ytzhao2023@users.noreply.github.com>
2024-02-02 22:25:18 -0800

    Alien-Invasion Games with Two Versions

---
+++ Alien Invasion_New/settings.py
@@ -0,0 +1,26 @@
+class Settings():
+    """The class stores all the settings in the game "Alien Invasion"."""
+    def __init__(self):
+        # Settings of screen.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        # Settings of the ship.
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # Settings of bullets.
+        self.bullet_speed_factor = 3
+        self.bullet_width = 30
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Settings of aliens.
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # Fleet_direction = 1 mean move right.
+        # Fleet_direction = -1 mean move left.
+        self.fleet_direction = 1
+
---
+++ README.md
@@ -0,0 +1,7 @@
+** Alien Invasion Game**
+
+Dive into a thrilling battle against extraterrestrial threats with this Alien Invasion game! This repository contains two versions:
+
+1. Classic Edition: Experience the original game inspired by the book "Python Crash Course, 3rd Edition." Defend your spaceship from waves of descending aliens in an authentic programming challenge.
+
+2. Enhanced Edition: Take the game to a new level with a dynamic twist! In this version, aliens enter the fight row by row after you clear each wave, adding a strategic layer to the gameplay.
